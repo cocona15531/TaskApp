ecc3c265c6e81e73725fb3f58b0ad6571d8e9c9b
issei.ueda <ron.ron633662@gmail.com>
2021-08-25 21:44:27 +0900

    category, 検索機能を追加

---
+++ Task.swift
@@ -0,0 +1,31 @@
+//
+//  Task.swift
+//  taskApp
+//
+//  Created by Issei Ueda on 2021/08/23.
+//
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    @objc dynamic var id = 0
+
+    // タイトル
+    @objc dynamic var title = ""
+
+    // 内容
+    @objc dynamic var contents = ""
+
+    // 日時
+    @objc dynamic var date = Date()
+
+    // id をプライマリーキーとして設定
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+    
+    //category
+    @objc dynamic var category = ""
+    
+}
